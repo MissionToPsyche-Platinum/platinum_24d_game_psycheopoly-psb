14ac491fc9c0ee4c7a984a21d21b326e28021d20
gazarrillo <gazarrillo1105@gmail.com>
2025-11-03 17:43:20 -0400

    fix: add tile map layer reference for piece movement

--- scripts/core/board.gd
+++ scripts/core/board.gd
@@ -9,6 +9,10 @@ func _ready() -> void:
 	var piece_scene = preload("res://scenes/Piece.tscn")
 	piece = piece_scene.instantiate()
 	add_child(piece)
+	
+	# Get reference to the TileMapLayer
+	var tile_map_layer = $TileMap/TileMapLayer
+	piece.tile_map = tile_map_layer
 
 	# Start the piece at position (10, 0) on the board (space 0 - GO)
 	piece.move_to(10, 0)
