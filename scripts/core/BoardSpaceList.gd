adf4a86f97facf396f8b6cf0b4a1e60ce9f2ba4c
John-K-Fugate <jkf5676@psu.edu>
2025-11-10 17:20:16 -0500

    Adjustments to board and property classes
    The gamestate class now autoloads and contains a board, which is a collection of game spaces. The attributes of these gamespaces can be accessed.

---
+++ scripts/core/BoardSpaceList.gd
@@ -0,0 +1,19 @@
+extends Resource
+
+class_name BoardSpaceList
+
+var TESTPROPERTY1 : PropertySpace = PropertySpace.new("Test Property 1", 20, 5, 10, 20, 25, 30, 5, 10)
+var TESTPROPERTY2 : PropertySpace = PropertySpace.new("Test Property 2", 25, 7, 12, 22, 28, 35, 5, 10)
+var TESTPROPERTY3 : PropertySpace = PropertySpace.new("Test Property 3", 30, 10, 15, 25, 32, 40, 5, 10)
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/core/GameState.gd
+++ scripts/core/GameState.gd
@@ -1,11 +1,26 @@
 extends Node
 
+var board: Array[GameSpace] = [] # contains all of the properties and game spaces on the board
+var spaces_list := BoardSpaceList.new() # fetches all of the game spaces from the resource
+
+# places all of the properties onto the board
+func _setUpBoard() -> void:
+	board.append(spaces_list.TESTPROPERTY1)
+	board.append(spaces_list.TESTPROPERTY2)
+	board.append(spaces_list.TESTPROPERTY3)
+
+	pass
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var p1 = PlayerState.new()
-	add_child(p1)
+	#var p1 = PlayerState.new()
+	#add_child(p1)
+	
+	_setUpBoard()
+	print(board[0]._property_name) # temprary to test output of accessing an element on the board
+
 	
 	pass # Replace with function body.
 
