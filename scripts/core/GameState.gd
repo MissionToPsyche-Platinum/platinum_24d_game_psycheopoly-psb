0ed0858fd28afb17a8f18fd602d317b4a2a2c483
John-K-Fugate <jkf5676@psu.edu>
2025-11-18 13:15:56 -0500

    Purchase property

--- scripts/core/GameState.gd
+++ scripts/core/GameState.gd
@@ -3,29 +3,58 @@ extends Node
 var board: Array[GameSpace] = [] # contains all of the properties and game spaces on the board
 var spaces_list := BoardSpaceList.new() # fetches all of the game spaces from the resource
 
+var PLAYER_COUNT = 4 # temporary, eventually we want to be able to set this before a game starts
+var players: Array[PlayerState] = []
+
+
 # places all of the properties onto the board
 func _setUpBoard() -> void:
 	board = spaces_list.board
 	pass
 
+func _setUpPlayers() -> void:
+	for i in range(PLAYER_COUNT):
+		players.append(PlayerState.new())
+		add_child(players[i])
+		players[i].balance = 1500 # temporary, change to constant (or however we choose to initialize values) in future
+	pass
+
+
 # changes the ownership of an ownable property
 func _transferProperty(property:Ownable, player:int) -> void:
-	property.player_owner = player
+	if (property._is_owned == false):
+		property._is_owned = true
+	property._player_owner = player
+	pass
+
+# adjusts balances of the player in the purchase and transfers ownership of a property. Used for when the player purchases an unowned property
+func _purchaseUnownedProperty(property:Ownable, player:int, purchase_price:int) -> void:
+	players[player].balance -= purchase_price
+	_transferProperty(property, player)
+	pass
+
+# adjusts balances of the player in the purchase and transfers ownership of a property. Used for any transaction where the player purchases an already owned property
+func _purchaseOwnedProperty(property:Ownable, buyer:int, seller:int, purchase_price:int) -> void:
+	players[buyer].balance -= purchase_price
+	players[seller].balance += purchase_price
+	_transferProperty(property, buyer)	
+	pass
+
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#var p1 = PlayerState.new()
-	#add_child(p1)
-	
 	_setUpBoard()
-	print(board[0]._property_name) # temprary to test output of accessing an element on the board
+	_setUpPlayers()
+	
+	# temporarily test the purchasing of properties
+	print(board[0]._player_owner) 
+	print(players[1].balance) 
 
-	# temporarily test the ownership of properties
-	print(board[0].player_owner) 
-	_transferProperty(board[0], 1)
-	print(board[0].player_owner) 
+	_purchaseUnownedProperty(board[0], 1, board[0]._initial_price)
+	print(board[0]._player_owner) 
+	print(players[1].balance) 
 
 
 	
