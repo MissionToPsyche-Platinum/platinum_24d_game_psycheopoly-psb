30a4e4ab430ecba0315ed0f0321e4f3d9b9d47e9
gazarrillo <gazarrillo1105@gmail.com>
2025-11-18 23:05:32 -0400

    feat: implement PropertyDetailsPopup and integrate with space info panel

--- scripts/core/space_info_panel.gd
+++ scripts/core/space_info_panel.gd
@@ -2,6 +2,7 @@ extends Control
 
 # Load space data
 const SpaceData = preload("res://scripts/core/space_data.gd")
+const PropertyDetailsPopup = preload("res://scenes/PropertyDetailsPopup.tscn")
 
 # References to UI elements
 @onready var panel_container: PanelContainer = $PanelContainer
@@ -15,6 +16,9 @@ const SpaceData = preload("res://scripts/core/space_data.gd")
 
 # Current space being displayed
 var current_space: int = 0
+
+# Popup instance
+var _details_popup: Control = null
 
 
 func _ready() -> void:
@@ -91,11 +95,24 @@ func update_space_display(space_num: int) -> void:
 
 # Called when the Details button is pressed
 func _on_details_pressed() -> void:
-	print("Details button pressed for space ", current_space)
-	# TODO: Open detailed property card or information window
-	# For now, just print the full space info
-	var space_info = SpaceData.get_space_info(current_space)
-	print("Space Info: ", space_info)
+	# Create popup if it doesn't exist
+	if _details_popup == null:
+		_details_popup = PropertyDetailsPopup.instantiate()
+		# Add to GameBoard scene (go up to find the root scene)
+		var game_board = get_tree().root.get_node("GameBoard")
+		if game_board:
+			game_board.add_child(_details_popup)
+			print("Popup added to GameBoard")
+		else:
+			get_tree().root.add_child(_details_popup)
+			print("Popup added to root")
+		print("Popup node path: ", _details_popup.get_path())
+	
+	# Show the popup with current space details
+	print("Showing details for space: ", current_space)
+	print("Popup visible before show: ", _details_popup.visible)
+	_details_popup.show_space_details(current_space, "Unowned")  # TODO: Get actual owner
+	print("Popup visible after show: ", _details_popup.visible)
 
 
 # Called when the Purchase button is pressed
