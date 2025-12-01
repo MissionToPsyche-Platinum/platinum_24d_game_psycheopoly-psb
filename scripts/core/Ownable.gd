0b1708de7b93db040e509819cacb0b7f6b272711
John-K-Fugate <jkf5676@psu.edu>
2025-12-01 10:52:16 -0500

    Removed unneccessary uses of "pass"

--- scripts/core/Ownable.gd
+++ scripts/core/Ownable.gd
@@ -4,12 +4,3 @@ class_name Ownable
 
 var _is_owned: bool = false # holds whether there is currently an owned property
 var _player_owner : int = 0 # holds which player owns the property
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
