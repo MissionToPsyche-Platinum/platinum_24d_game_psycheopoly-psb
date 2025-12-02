b39496850dd2c341eb4b5c6dfe8f5f8f8fed8be0
John-K-Fugate <jkf5676@psu.edu>
2025-12-02 15:35:56 -0500

    Initial work on merging gamestates

--- scripts/core/game_state.gd
+++ scripts/core/game_state.gd
@@ -30,6 +30,55 @@ signal player_money_updated(player)
 ## Default to "Normal" so Start Menu can initialize its OptionButton.
 var difficulty: String = "Normal"
 
+# contains the board data model
+var board: Array[GameSpace] = [] 
+
+# holds the list of board spaces
+var spaces_list := BoardSpaceList.new() 
+
+# temporary, eventually we want to be able to set this before a game starts
+var PLAYER_COUNT = 4 
+
+# holds the playerstate data models
+var players: Array[PlayerState] = []
+
+# places all of the properties onto the board
+func _setUpBoard() -> void:
+	board = spaces_list.board
+
+func _setUpPlayers() -> void:
+	for i in range(PLAYER_COUNT):
+		players.append(PlayerState.new())
+		add_child(players[i])
+		players[i].balance = 1500 # temporary, change to constant (or however we choose to initialize values) in future
+
+
+# changes the ownership of an ownable property
+func _transferProperty(property:Ownable, player:int) -> void:
+	if (property._is_owned == false):
+		property._is_owned = true
+	property._player_owner = player
+
+# adjusts balances of the player in the purchase and transfers ownership of a property. Used for when the player purchases an unowned property
+func _purchaseUnownedProperty(property:Ownable, player:int, purchase_price:int) -> void:
+	players[player].balance -= purchase_price
+	_transferProperty(property, player)
+
+# adjusts balances of the player in the purchase and transfers ownership of a property. Used for any transaction where the player purchases an already owned property
+func _purchaseOwnedProperty(property:Ownable, buyer:int, seller:int, purchase_price:int) -> void:
+	players[buyer].balance -= purchase_price
+	players[seller].balance += purchase_price
+	_transferProperty(property, buyer)	
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_setUpBoard()
+	_setUpPlayers()
+
+
 
 func set_difficulty(new_difficulty: String) -> void:
 	## Simple setter used by StartMenu.gd
