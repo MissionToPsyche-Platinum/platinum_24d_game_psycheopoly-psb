bd2478062b02f9e56aefcab7facfea5834886aac
JayBar93 <computingbaris@gmail.com>
2026-02-16 14:36:42 -0500

    Big Chance Card Update
    Cards are mostly functioning. edge case handling still not perfect. Presentable form for now. Below are noted bugs that need fixing:
    - Edit format of text in the windows
    - finish Metal cards (16 and 17) for pay based on data points/discovery
    - jump or teleport movement for forward movement
    - multiplier for payment result of getting Sci.Instr. or Planet
    - still says "just visiting" message after being sent to "jail"
    - Getting a get out of jail free card locks it but no unlock exists
    	- need a way for players to have it in their inventory
    	- need logic for using the card while in "jail" and releasing the lock

---
+++ scripts/core/chance_card_manager.gd
@@ -0,0 +1,123 @@
+extends Node
+ 
+const Board = preload("res://scripts/core/board.gd")
+
+signal request_move_forward(spaces: int)
+signal request_teleport_movement(space: int)
+
+var player_count = -1
+var current_player = -1
+var other_player = -1
+var forward_movement = -1
+var go_for_launch1_available : bool = true
+var go_for_launch1_owner : int = -1
+var go_for_launch2_available : bool = true
+var go_for_launch2_owner : int = -1
+
+
+func resolve_card(card_num: int, money_value: int, movement_value: int, space_number: int) -> void:
+	current_player = GameState.current_player_index
+	player_count = GameState.player_count
+	var passed_go = 200
+
+	if card_num in range(0, 10): #cards with a flat earning
+		GameState.credit(current_player,money_value) 
+	elif card_num in range(10,14): #cards with a flat loss
+		GameState.debit(current_player,money_value) 
+	elif card_num == 14: #pay each player 50
+		var pay_opponents = (player_count - 1) * 50
+		GameState.debit(current_player,pay_opponents) 
+		
+		var paid_player = (current_player + 1) % player_count
+		while paid_player != current_player:
+			GameState.credit(paid_player, 50) 
+			paid_player = (paid_player + 1) % player_count
+
+	elif card_num == 15: #earn 10 from each player
+		var earn_from_opponents = (player_count - 1) * 10
+		GameState.credit(current_player,earn_from_opponents) 
+		
+		var losing_player = (current_player + 1) % player_count
+		while losing_player != current_player:
+			GameState.debit(losing_player, 10) 
+			losing_player = (losing_player + 1) % player_count
+			
+	elif card_num == 16: #pay 45 per data point, pay 120 per discovery
+		
+		
+		
+		pass
+	elif card_num == 17: #pay 25 per data point, pay 100 per discovery
+		pass
+		
+		##Cases for silicate cards that are movement based
+	elif card_num in range(18, 28): #advance player to specific property
+		if space_number < movement_value:
+			forward_movement = movement_value - space_number
+		
+		elif space_number > movement_value:
+			forward_movement = (40 - space_number) + movement_value
+			GameState.credit(current_player, passed_go) 
+		
+		emit_signal ("request_move_forward", forward_movement)
+		
+	elif card_num in [28, 29]: #move to nearest scientific instrument
+		var instrument1_distance = abs(space_number - 5)
+		var instrument2_distance = abs(space_number - 15)
+		var instrument3_distance = abs(space_number - 25)
+		var instrument4_distance = abs(space_number - 35)
+		var instrucment_movement = min(instrument1_distance, instrument2_distance, instrument3_distance, instrument4_distance)
+		
+		if instrucment_movement == instrument1_distance:
+			emit_signal ("request_teleport_movement", 5)
+		elif instrucment_movement == instrument2_distance:
+			emit_signal ("request_teleport_movement", 15)
+		elif instrucment_movement == instrument3_distance:
+			emit_signal ("request_teleport_movement", 25)
+		elif instrucment_movement == instrument4_distance:
+			emit_signal ("request_teleport_movement", 35)
+	
+	elif card_num == 30: #move to nearest planet
+		var mars_distance = abs(space_number - 12)
+		var jupiter_distance = abs(space_number - 27)
+		
+		if mars_distance < jupiter_distance:
+			emit_signal ("request_teleport_movement", 12)
+		else:
+			emit_signal ("request_teleport_movement", 27)
+		
+	elif card_num == 31: #move back 3 spaces
+		var backward_movement = -1
+		
+		if space_number <= 2:
+			backward_movement = (space_number - 3) + 40
+		else:
+			backward_movement = space_number - 3
+		
+		emit_signal ("request_teleport_movement", backward_movement)
+		
+	elif card_num in [32, 33]: #move directly to jail
+		emit_signal ("request_teleport_movement", movement_value) ##movement works but says "Just visiting" after movement
+		
+	elif card_num == 34: #get out of jail card (needs a lock for inventory)
+		go_for_launch1_available = false
+		go_for_launch1_owner = current_player
+		##player_data[current_player].go_for_launch_cards += 1
+	
+	elif card_num == 35: #get out of jail card (needs a lock for inventory)
+		go_for_launch2_available = false
+		go_for_launch2_owner = current_player
+		##player_data[current_player].go_for_launch_cards += 1
+	else: #in place of an error
+		pass
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
